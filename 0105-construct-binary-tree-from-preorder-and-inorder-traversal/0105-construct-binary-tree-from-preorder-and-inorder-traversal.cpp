36ffe7beb5125304fb82df91ef0cdc96fc43991a
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-12 23:25:22 +0530

    Time: 10 ms (18.63%), Space: 27.2 MB (81.32%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    TreeNode* build(vector<int>& pre, int preLo, int preHi, vector<int>& in, int inLo, int inHi) {
+        if(preLo > preHi) return NULL;
+        TreeNode* root = new TreeNode(pre[preLo]);
+        if(preLo == preHi) return root;
+        int i = inLo;
+        while(i<inHi) {
+            if(in[i] == pre[preLo]) break;
+            i++;
+        }
+        int lCount = i - inLo;
+        int rCount = inHi-1;
+        root->left = build(pre, preLo+1, preLo+lCount, in, inLo, i-1);
+        root->right = build(pre, preLo+lCount+1, preHi, in, i+1, inHi);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+        int n = pre.size();
+        return build(pre, 0, n-1, in, 0, n-1);
+    }
+};
