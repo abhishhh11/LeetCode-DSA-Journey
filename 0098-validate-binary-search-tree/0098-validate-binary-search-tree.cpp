1528cd7e525d09da15c369b63a42a66b6f6ed9c9
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-13 14:13:15 +0530

    Time: 0 ms (100%), Space: 22.2 MB (8.3%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    long long maxTree(TreeNode* root) {
-        if(root == NULL) return LLONG_MIN;
-        return max((long long) (root->val), max(maxTree(root->left), maxTree(root->right))) ;
-    }
-    long long minTree(TreeNode* root) {
-        if(root == NULL) return LLONG_MAX;
-        return min((long long) (root->val), min(minTree(root->left), minTree(root->right)));
+    // long long maxTree(TreeNode* root) {
+    //     if(root == NULL) return LLONG_MIN;
+    //     return max((long long) (root->val), max(maxTree(root->left), maxTree(root->right))) ;
+    // }
+    // long long minTree(TreeNode* root) {
+    //     if(root == NULL) return LLONG_MAX;
+    //     return min((long long) (root->val), min(minTree(root->left), minTree(root->right)));
+    // }
+    // bool isValidBST(TreeNode* root) {
+    //     if(root == NULL) return true;
+    //     //else if(root->left == NULL && root->right == NULL) return true;
+    //     else if((long long) (root->val) <= maxTree(root->left)) return false;
+    //     else if((long long) (root->val) >= minTree(root->right)) return false;
+    //     else return isValidBST(root->left) && isValidBST(root->right) ;
+    // }
+    void inOrd(TreeNode* root, vector<int>& ans) {
+        if(root == NULL) return;
+        inOrd(root->left, ans);
+        ans.push_back(root->val);
+        inOrd(root->right, ans);
     }
     bool isValidBST(TreeNode* root) {
-        if(root == NULL) return true;
-        //else if(root->left == NULL && root->right == NULL) return true;
-        else if((long long) (root->val) <= maxTree(root->left)) return false;
-        else if((long long) (root->val) >= minTree(root->right)) return false;
-        else return isValidBST(root->left) && isValidBST(root->right) ;
+        vector<int> ans;
+        inOrd(root, ans);
+        for(int i = 1; i<ans.size();i++){
+            if(ans[i] <= ans[i-1]) return false;
+        }
+        return true;
     }
 };
