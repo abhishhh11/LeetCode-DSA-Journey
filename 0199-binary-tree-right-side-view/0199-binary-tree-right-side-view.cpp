c9df0bae03dc055dd4c7290701457746dc78ae1d
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-13 10:07:56 +0530

    Time: 0 ms (100%), Space: 14.8 MB (85.6%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,29 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int levels(TreeNode* root) {
         if(root == NULL) return 0;
         return 1 + max(levels(root->left), levels(root->right));
+
     }
-    void preOrder(TreeNode* root, vector<int>& ans, int level) {
-        if(root == NULL) return; 
+    void pre(TreeNode* root,vector<int>& ans, int level) {
+        if(root == NULL) return;
         ans[level] = root->val;
-        preOrder(root->left, ans, level+1);
-        preOrder(root->right, ans, level+1);
+        pre(root->left, ans, level+1);
+        pre(root->right, ans, level+1);
     }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans(levels(root),0);
-        preOrder(root, ans, 0);
+        vector<int> ans(levels(root), 0);
+        pre(root, ans, 0);
         return ans;
     }
 };
