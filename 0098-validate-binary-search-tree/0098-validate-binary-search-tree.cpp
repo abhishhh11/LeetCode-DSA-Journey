27428a12125ded174b83563c32a158fed3cbcd1c
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-13 14:24:41 +0530

    Time: 0 ms (100%), Space: 22.1 MB (15.9%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// T.C = O(n^2)    S.C == O(1)
     // long long maxTree(TreeNode* root) {
     //     if(root == NULL) return LLONG_MIN;
     //     return max((long long) (root->val), max(maxTree(root->left), maxTree(root->right))) ;
@@ -15,18 +16,41 @@ public:
     //     else if((long long) (root->val) >= minTree(root->right)) return false;
     //     else return isValidBST(root->left) && isValidBST(root->right) ;
     // }
-    void inOrd(TreeNode* root, vector<int>& ans) {
+
+// TC = O(n)    SC == O(n)
+ 
+    // void inOrd(TreeNode* root, vector<int>& ans) {
+    //     if(root == NULL) return;
+    //     inOrd(root->left, ans);
+    //     ans.push_back(root->val);
+    //     inOrd(root->right, ans);
+    // }
+    // bool isValidBST(TreeNode* root) {
+    //     vector<int> ans;
+    //     inOrd(root, ans);
+    //     for(int i = 1; i<ans.size();i++){
+    //         if(ans[i] <= ans[i-1]) return false;
+    //     }
+    //     return true;
+    // }
+
+// TC == O(n)    SC == O(1)
+    TreeNode* prev = NULL;
+    bool flag = true;
+    void inOrd(TreeNode* root) {
         if(root == NULL) return;
-        inOrd(root->left, ans);
-        ans.push_back(root->val);
-        inOrd(root->right, ans);
+        inOrd(root->left);
+        if(prev != NULL) {
+            if(root->val <= prev->val) {
+                flag = false;
+                return;
+            }
+        }
+        prev = root;
+        inOrd(root->right);
     }
     bool isValidBST(TreeNode* root) {
-        vector<int> ans;
-        inOrd(root, ans);
-        for(int i = 1; i<ans.size();i++){
-            if(ans[i] <= ans[i-1]) return false;
-        }
-        return true;
-    }
+        inOrd(root);
+        return flag;
+    }    
 };
