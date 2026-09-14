def0d9b7ded14596a1dd5008a2c958a05a716c0c
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-14 18:34:08 +0530

    Time: 0 ms (100%), Space: 11 MB (35.01%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,49 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-// RECURSIVE METHOD
-
-    // void inOrder(TreeNode* root, vector<int> &in) {
-    //     if(root == NULL) return ;
-    //     inOrder(root->left, in);
-    //     in.push_back(root->val);
-    //     inOrder(root->right, in);
-    // }
-    // vector<int> inorderTraversal(TreeNode* root) {
-    //     vector<int> in;
-    //     inOrder(root, in);
-    //     return in;
-    // }
-
-// ITERATIVE METHOD
-
     vector<int> inorderTraversal(TreeNode* root) {
+// MORRIS TRAVERSAL ->  it is best because it's space commplexity is constant -> O(1) 
+// except this all traversal have space comp O(n)
         vector<int> ans;
         if(root == NULL) return ans;
-        
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-        //st.push(root);
-        while(st.size() != 0 || node != NULL ) {
-            if( node != NULL) {
-                st.push(node);
-                node = node->left;
+        TreeNode* curr = root;
+        TreeNode* pred;
+        while(curr != NULL){
+            if(curr->left != NULL) {
+                pred = curr->left;
+
+                while(pred->right != NULL && pred->right != curr) {
+                    pred = pred->right;
+                }
+                
+                if(pred->right == NULL) {
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+
+                if(pred->right == curr) {
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
-            else { // Now node is null
-                TreeNode* temp = st.top();
-                st.pop();
-                ans.push_back(temp->val);
-                node = temp->right;
+            else {
+                ans.push_back(curr->val);
+                curr = curr->right;
             }
         }
         return ans;
