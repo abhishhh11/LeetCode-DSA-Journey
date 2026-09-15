2802c4264af0d5a127d5d0a9c269d79779fd9a6f
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-15 15:31:10 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
+    // bool isAnagram(string s, string t) {
+    //     if(s.length() != t.length()) return false;
+    //     unordered_map<char,int> mp1;
+    //     unordered_map<char,int> mp2;
+    //     for(int i=0;i<s.length();i++) {
+    //         mp1[s[i]]++;
+    //     }
+    //     for(int i=0;i<t.length();i++) {
+    //         mp2[t[i]]++;
+    //     }
+    //     for(auto x : mp1) {
+    //         char ch1 = x.first;
+    //         int freq1 = x.second;
+    //         if(mp2.find(ch1) != mp2.end()) {
+    //             int freq2 = mp2[ch1];
+    //             if(freq1 != freq2) return false; 
+    //         }
+    //         else return false;
+    //     }
+    //     return true;
+    // }
+
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
+        unordered_map<char,int> mp;
         for(int i=0;i<s.length();i++) {
-            mp1[s[i]]++;
+            mp[s[i]]++;
         }
         for(int i=0;i<t.length();i++) {
-            mp2[t[i]]++;
-        }
-        for(auto x : mp1) {
-            char ch1 = x.first;
-            int freq1 = x.second;
-            if(mp2.find(ch1) != mp2.end()) {
-                int freq2 = mp2[ch1];
-                if(freq1 != freq2) return false; 
+            char ch = t[i];
+            if(mp.find(ch) != mp.end()) {
+                mp[ch]--;
+                if(mp[ch] == 0) mp.erase(ch);
             }
             else return false;
         }
-        return true;
+        if(mp.size() > 0) return false;
+        else return true;
     }
+
 };
