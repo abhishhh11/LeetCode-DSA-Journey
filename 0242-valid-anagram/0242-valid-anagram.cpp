44d802c663e726aff75b1583618fc8e4e7dc03fe
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-18 20:21:57 +0530

    Time: 7 ms (18.25%), Space: 9.7 MB (48.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -22,22 +22,29 @@ public:
     //     return true;
     // }
 
+    // bool isAnagram(string s, string t) {
+    //     if(s.length() != t.length()) return false;
+    //     unordered_map<char,int> mp;
+    //     for(int i=0;i<s.length();i++) {
+    //         mp[s[i]]++;
+    //     }
+    //     for(int i=0;i<t.length();i++) {
+    //         char ch = t[i];
+    //         if(mp.find(ch) != mp.end()) {
+    //             mp[ch]--;
+    //             if(mp[ch] == 0) mp.erase(ch);
+    //         }
+    //         else return false;
+    //     }
+    //     if(mp.size() > 0) return false;
+    //     else return true;
+    // }
+
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        unordered_map<char,int> mp;
-        for(int i=0;i<s.length();i++) {
-            mp[s[i]]++;
-        }
-        for(int i=0;i<t.length();i++) {
-            char ch = t[i];
-            if(mp.find(ch) != mp.end()) {
-                mp[ch]--;
-                if(mp[ch] == 0) mp.erase(ch);
-            }
-            else return false;
-        }
-        if(mp.size() > 0) return false;
-        else return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(), t.end());
+        if(s == t) return true;
+        else return false;
     }
 
 };
