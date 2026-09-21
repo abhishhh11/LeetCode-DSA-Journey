509c16e5a7dfbea8397d97700a11fe68c239a2ff
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-21 19:09:55 +0530

    Time: 13 ms (31.66%), Space: 7.8 MB (65.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-    int myFib(int n) {
-        if(n<2) return n;
-        else return myFib(n-1) + myFib(n-2);
-    }
-
     int fib(int n) {
-        return myFib(n);
+        if(n<2) return n;
+        else return fib(n-1) + fib(n-2);
     }
 };
