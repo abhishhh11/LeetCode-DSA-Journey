f5530617ae10652e6123a65d5d151bc21335c9a2
Abhishek Yadav <vishuyadavindian@gmail.com>
2026-09-22 23:14:35 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
     vector<int> dp;
-    int ftd(vector<int> arr, int i) {
-        if(i == arr.size()-1) return arr[i];
-        if(i == arr.size()-2) return max(arr[i], arr[i+1]);
-        if(dp[i] != -1) return dp[i];
+
+    int f(vector<int> arr, int i) { // simple recursive call
+        if(i == arr.size() -1 ) return arr[i];// single house remaining
+        if(i == arr.size() - 2) return max(arr[i], arr[i+1]); // 2 house remaining
+        return max(arr[i] + f(arr, i+2), 0 + f(arr, i+1));
+    }
+
+    int ftd(vector<int> arr, int i) {// function of top down approach of dp
+        if(i == arr.size()-1) return arr[i]; // single house remaining 
+        if(i == arr.size()-2) return max(arr[i], arr[i+1]); // 2 house remaining
+        if(dp[i] != -1) return dp[i]; // ith value of dp is not computed yet
         return dp[i] = max(arr[i] + ftd(arr, i+2), 0 + ftd(arr, i+1));
     }
-    int rob(vector<int>& nums) {
+
+    int fbu(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 1) return arr[0];
         dp.clear();
-        dp.resize(105, -1);
-        return ftd(nums, 0);
+        dp.resize(n);
+// Base cases,
+        dp[n-1] = arr[n-1];
+        dp[n-2] = max(arr[n-1], arr[n-2]);
+        for(int i = n-3;i>=0;i--) {
+            dp[i] = max(arr[i] + dp[i+2], 0 + dp[i+1]);
+        }
+        return dp[0];
+    }
+
+    int rob(vector<int>& nums) {
+        // dp.clear();
+        // dp.resize(105, -1);
+        //return f(nums, 0);
+        //return ftd(nums, 0);
+        return fbu(nums);
     }
 };
